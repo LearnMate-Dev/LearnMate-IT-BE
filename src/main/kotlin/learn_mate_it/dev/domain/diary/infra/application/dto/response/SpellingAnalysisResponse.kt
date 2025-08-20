73dcdec50609e86ce6a653110005a6b30bbf19f6
daeun084 <030804jk@naver.com>
2025-08-20 16:05:03 +0900

    refactor: mod analysis spelling response dto

--- src/main/kotlin/learn_mate_it/dev/domain/diary/infra/application/dto/response/SpellingAnalysisResponse.kt
+++ src/main/kotlin/learn_mate_it/dev/domain/diary/infra/application/dto/response/SpellingAnalysisResponse.kt
@@ -1,55 +1,33 @@
 package learn_mate_it.dev.domain.diary.infra.application.dto.response
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.fasterxml.jackson.annotation.JsonProperty
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 data class SpellingAnalysisResponse(
-    val requestId: String?,
-    val result: Int,
-    @JsonProperty("return_object")
-    val returnObject: AnalysisObject
+    val origin: String,
+    val revised: String,
+    val revisedSentences: List<RevisedSentence>?
 )
 
-data class AnalysisObject(
-    @JsonProperty("doc_id")
-    val docId: String,
+data class RevisedSentence(
+    val origin: String,
+    val revised: String,
+    val revisedBlocks: List<RevisedBlock>?,
+)
+
+data class RevisedBlock(
+    val origin: Origin,
+    val revised: String,
+    val revisions: List<Revision>
+)
+
+data class Origin(
+    val content: String,
+    val beginOffset: Int,
+    val length: Int
+)
+
+data class Revision(
+    val revised: String,
     val category: String,
-    val sentence: List<SentenceAnalysis>
-)
-
-data class SentenceAnalysis(
-    val id: Int,
-    @JsonProperty("reserve_str")
-    val reserveStr: String,
-    val text: String, // origin sentence text
-    val morp: List<Morp>, // analysis result
-    @JsonProperty("morp_eval")
-    val morpEval: List<MorpEval>,
-    val word: List<Word>,
-)
-
-data class Morp(
-    val id: Int,
-    val lemma: String, // 음절
-    val type: String,
-    val position: Int,
-    val weight: Double
-)
-
-data class MorpEval(
-    val id: Int,
-    val result: String,
-    val target: String, // lemma
-    val wordId: Int,
-    val mBegin: Int,
-    val mEnd: Int
-)
-
-data class Word(
-    val id: Int,
-    val text: String, // lemma
-    val type: String,
-    val begin: Int,
-    val end: Int
+    val comment: String,
+    val examples: List<String>
 )
